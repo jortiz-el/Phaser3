00ba0738e01803c16f8677088d653ea5c3a0a874
Joao Ortiz <ojoao_funkyman@hotmail.com>
2021-03-15 22:18:56 +0100

    primer juego

---
+++ getStartedPhaser/src/app/game/game.component.ts
@@ -0,0 +1,161 @@
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+
+export class GameComponent implements OnInit {
+
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [ MainScene ],
+      parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 300 },
+          debug: false
+        }
+      }
+    };
+  }
+
+  ngOnInit(): void {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
+
+class MainScene extends Phaser.Scene {
+  
+  platforms;
+  player;
+  cursors;
+  stars;
+  score = 0;
+  scoreText;
+  
+  constructor() {
+    super({ key: 'main' });
+  }
+  
+  create() {
+    //console.log('create method');
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.add.image(400, 300, 'sky');
+
+    this.platforms = this.physics.add.staticGroup();
+
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    //añadir colision entre el jugador y las plataformas
+    this.physics.add.collider(this.player, this.platforms);
+
+    this.stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+    
+    this.stars.children.iterate(function (child) {
+    
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    
+    });
+
+    this.physics.add.collider(this.stars, this.platforms);
+    //comprobacion si player colisiona con alguna estrella y ejecuta funcion collectstar
+    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+
+    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', color: '#000' });
+  }
+
+  preload() {
+    //console.log('preload method');
+    this.load.image('sky', 'assets/images/sky.png');
+    this.load.image('ground', 'assets/images/platform.png');
+    this.load.image('star', 'assets/images/star.png');
+    this.load.image('bomb', 'assets/images/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/images/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+  }
+
+  update() {
+    //console.log('update method');
+    if (this.cursors.left.isDown)
+    {
+        this.player.setVelocityX(-160);
+
+        this.player.anims.play('left', true);
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.player.setVelocityX(160);
+
+        this.player.anims.play('right', true);
+    }
+    else
+    {
+        this.player.setVelocityX(0);
+
+        this.player.anims.play('turn');
+    }
+    
+    if (this.cursors.up.isDown && this.player.body.touching.down)
+    {
+        this.player.setVelocityY(-330);
+    }
+
+  }
+
+  collectStar (player, star)
+  {
+      star.disableBody(true, true);
+
+      this.score += 10;
+      this.scoreText.setText('Score: ' + this.score);
+  }
+
+}
+
+
